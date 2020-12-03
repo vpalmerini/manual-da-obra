3be677fa1cdb80589133f32f819af66de2a80915
Victor Palmerini <victorpalmerini@gmail.com>
2020-12-02 21:05:27 -0300

    backend initial setup

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+var cors = require("cors");
+
+const app = express();
+
+const { UI_URL, PORT } = process.env;
+const port = PORT;
+const basePath = "/api";
+
+app.use(cors({ credentials: true, origin: UI_URL }));
+
+app.use(express.json());
+
+app.get(`${basePath}/ping`, (req, res) => {
+  res.send("pong");
+});
+
+console.log(`API is running in http://localhost:${port}/api`)
+
+app.listen(port);
