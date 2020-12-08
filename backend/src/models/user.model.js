683c217afab7402f257de88899ecfc5b6f2e9f81
Victor Palmerini <victorpalmerini@gmail.com>
2020-12-08 10:38:03 -0300

    feat: add User model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  return next();
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
