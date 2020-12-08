116285aeff622c913fb41431d5844146f037a1a3
Victor Palmerini <victorpalmerini@gmail.com>
2020-12-07 22:07:21 -0300

    feat: add state to Login view

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Card, Input } from "antd";
+import { Card, Input, Button } from "antd";
 import Crane from "assets/images/crane.svg";
 import styles from "./Login.module.scss";
 
-const Login = () => (
-  <div className={styles.container}>
-    <div className={styles.card}>
-      <Card style={{ width: 300 }}>
-        <img src={Crane} alt="crane-logo" />
-        <div className={styles.input}>
-          <Input placeholder="Username" />
-        </div>
-        <div className={styles.input}>
-          <Input placeholder="Senha" />
-        </div>
-      </Card>
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <Card style={{ width: 300 }}>
+          <img src={Crane} alt="crane-logo" />
+          <h1>Manual da Obra</h1>
+          <form action="">
+            <div className={styles.input}>
+              <Input placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div className={styles.input}>
+              <Input placeholder="Senha" value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+            </div>
+            <Button type="primary">Entrar</Button>
+          </form>
+        </Card>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
