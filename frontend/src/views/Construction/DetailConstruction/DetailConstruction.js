946a839eff29b577714360b56fa02b95369c1cbc
Victor Palmerini <victorpalmerini@gmail.com>
2020-12-12 20:50:46 -0300

    feat: integrates system listing

--- frontend/src/views/Construction/DetailConstruction/DetailConstruction.js
+++ frontend/src/views/Construction/DetailConstruction/DetailConstruction.js
@@ -4,6 +4,7 @@ import { detail } from "services/construction.service";
 
 import Page from "components/Page/Page";
 import Container from "components/Container/Container";
+import Card from "components/Card/Card";
 import { Spin } from "antd";
 import { toast } from "react-toastify";
 
@@ -12,6 +13,7 @@ import styles from "./DetailConstruction.module.scss";
 const DetailConstruction = ({ match }) => {
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
+  const [systems, setSystems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -19,9 +21,10 @@ const DetailConstruction = ({ match }) => {
       setIsLoading(true);
       detail(id)
         .then((response) => {
-          const { name, location } = response.data.construction;
+          const { name, location, systems } = response.data.construction;
           setName(name);
           setLocation(location);
+          setSystems(systems);
         })
         .catch(() => {
           toast.error("Ops! Aconteceu algum erro pra pegar os dados da obra");
@@ -43,6 +46,19 @@ const DetailConstruction = ({ match }) => {
             </div>
             <div className={styles.description}>
               <h3>{location}</h3>
+            </div>
+            <div className={styles.systems}>
+              {systems && systems.length > 0 ? (
+                systems.map((sys) => (
+                  <div className={styles.card}>
+                    <Card
+                      id={sys._id}
+                      title={sys.name}
+                      description={sys.description}
+                    />
+                  </div>
+                ))
+              ) : <h4>Nenhum sistema cadastrado</h4>}
             </div>
           </div>
         )}
