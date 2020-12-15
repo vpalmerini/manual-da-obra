e60fa7c056e62810e08d440ada9307095fff06c3
Victor Palmerini <victorpalmerini@gmail.com>
2020-12-14 21:21:10 -0300

    feat: add logging to API

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const rfs = require("rotating-file-stream");
+
 const connectDB = require("./src/db");
 const { handleError } = require("./src/helpers/error");
 
 const app = express();
 
-const { UI_URL, PORT } = process.env;
+const { UI_URL, PORT, NODE_ENV } = process.env;
 const port = PORT;
 const basePath = "/api";
+
+if (NODE_ENV === "production") {
+  const accessLogStream = rfs.createStream('access.log', {
+    interval: '7d',
+    path: path.join(__dirname, 'log')
+  });
+  app.use(morgan('combined', { stream: accessLogStream }));
+} else {
+  app.use(morgan('dev'));
+}
+
 
 app.use(cors({ credentials: true, origin: UI_URL }));
 
