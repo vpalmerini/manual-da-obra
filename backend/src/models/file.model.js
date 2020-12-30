640c5ced58e695aca61758098830e3b9aae0c4bb
Victor Palmerini <victorpalmerini@gmail.com>
2020-12-30 17:29:13 -0300

    docs: add schema to File model

--- backend/src/models/file.model.js
+++ backend/src/models/file.model.js
@@ -2,6 +2,37 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      File:
+ *        type: object
+ *        required:
+ *          - name
+ *        properties:
+ *          _id:
+ *            type: string
+ *            description: The auto-generated id of the file
+ *          name:
+ *            type: string
+ *          type:
+ *            type: string
+ *            enum: ["video", "project"]
+ *          url:
+ *            type: string
+ *            description: URL of the object in S3 (AWS)
+ *          key:
+ *            type: string
+ *            description: Just a hash to add to object's name
+ *          createdAt:
+ *            type: date
+ *            format: date-time
+ *          system:
+ *            type: object
+ *            $ref: '#/components/schemas/System'
+ */ 
+
 const FileSchema = new Schema({
   name: {
     type: String,
